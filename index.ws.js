34d10d3a9136f15a8d01b81f89557c991d44f15e
JASON SUAREZ <suarezjason@icloud.com>
2025-02-28 17:58:37 +0100

    refact: implement the websocket server and add the client

--- index.ws.js
+++ index.ws.js
@@ -5,10 +5,36 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
-
 });
 
 server.on("request", app);
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+const { WebSocketServer } = require("ws");
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  const numClients = wss.clients.size;
+  console.log(`Client connected, total: ${numClients}`);
+
+  wss.broadcast(`
+            Current Visitors Online: ${numClients}
+        `);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send(` Welcome to FSFE `);
+  }
+  if (ws.readyState === ws.CLOSED) {
+    wss.broadcast(`Current Visitors Online: ${numClients}`);
+    console.log("Client disconnected");
+  }
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach((client) => {
+    client.send(data);
+  });
+};
