f797083839bec4c286152c2511256a4458ddf02e
JASON SUAREZ <suarezjason@icloud.com>
2025-03-01 09:41:04 +0100

    add database logging"

--- index.ws.js
+++ index.ws.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { get } = require("http");
 const server = require("http").createServer();
 const port = process.env.PORT || 3000;
 const app = express();
@@ -10,6 +11,19 @@ app.get("/", (req, res) => {
 server.on("request", app);
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+});
+
+process.on("SIGINT", () => {
+  wss.clients.forEach((client) => {
+    client.close();
+  });
+  server.close(() => {
+    console.log("\n Server closing");
+
+    wss.close();
+    shutdown();
+    process.exit(0);
+  });
 });
 
 const { WebSocketServer } = require("ws");
@@ -27,14 +41,26 @@ wss.on("connection", (ws) => {
   if (ws.readyState === ws.OPEN) {
     ws.send(` Welcome to FSFE `);
   }
+
+  db.run(
+    `
+        INSERT INTO visitors (count, time)
+        VALUES (?, datetime('now'))
+        `,
+    [numClients]
+  );
+
   if (ws.readyState === ws.CLOSED) {
     wss.broadcast(`Current Visitors Online: ${numClients}`);
     console.log("Client disconnected");
+    getCounts();
   }
-});
 
-wss.on("close", () => {
-  console.log("Server closed");
+  // Listen for client disconnection
+  ws.on("close", () => {
+    console.log("Client disconnected");
+    getCounts();
+  });
 });
 
 wss.broadcast = function broadcast(data) {
@@ -44,3 +70,32 @@ wss.broadcast = function broadcast(data) {
 };
 
 /* DB */
+const sqlite = require("sqlite3").verbose();
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(
+    `
+        CREATE TABLE IF NOT EXISTS visitors (
+            count INTEGER,
+            time DATETIME
+        )
+        `
+  );
+});
+
+function getCounts() {
+  return db.each(`SELECT * FROM visitors`, (err, row) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(row);
+    }
+  });
+}
+
+function shutdown() {
+  getCounts();
+  console.log("Shutting down db");
+  db.close();
+}
